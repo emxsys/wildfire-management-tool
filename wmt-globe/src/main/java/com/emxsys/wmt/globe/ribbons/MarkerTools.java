9d834b63d0d98bd6964cf3404232da0cb9070ac5
Bruce Schubert <devnull@localhost>
2014-04-09 14:07:51 -0700

    Globe: Added Center On action to Marker Tools

--- wmt-globe/src/main/java/com/emxsys/wmt/globe/ribbons/MarkerTools.java
+++ wmt-globe/src/main/java/com/emxsys/wmt/globe/ribbons/MarkerTools.java
@@ -5,37 +5,59 @@
  */
 package com.emxsys.wmt.globe.ribbons;
 
-import com.emxsys.wmt.globe.markers.BasicMarkerNode;
+import com.emxsys.wmt.globe.markers.BasicMarker;
 import com.terramenta.ribbon.RibbonManager;
 import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
+import org.openide.util.NbBundle.Messages;
 import org.openide.util.Utilities;
 import org.pushingpixels.flamingo.api.ribbon.JRibbon;
 import org.pushingpixels.flamingo.api.ribbon.RibbonContextualTaskGroup;
 
 /**
  * The MarkerTools instance listens for the existence of a BasicMarkerNode object on the lookup, and
- * if found it displays the "Layer Tools" contextual task group.
+ * if found it displays the "Marker Tools" contextual task group.
  * <p>
  * NOTE: The singleton must be instantiated for this contextual menu to work. You should call
- * MarkerTools.getInstnace() in the Globe initializeResources() body.
- * 
+ * MarkerTools.getInstance() in the Globe initializeResources() body.
+ *
  * @author Bruce Schubert
  */
+@Messages({
+    "CTL_MarkerTools=Marker Tools"
+})
 public final class MarkerTools implements LookupListener {
 
     private RibbonContextualTaskGroup taskGroup;
-    private final Lookup.Result<BasicMarkerNode> lookupResult;
+    private final Lookup.Result<BasicMarker> lookupResult;
 
     @SuppressWarnings("LeakingThisInConstructor")
     private MarkerTools() {
         // Initilize the lookup listener
-        this.lookupResult = Utilities.actionsGlobalContext().lookupResult(BasicMarkerNode.class);
+        this.lookupResult = Utilities.actionsGlobalContext().lookupResult(BasicMarker.class);
         this.lookupResult.addLookupListener(this);
-
         // Trigger initial show/hide
         resultChanged(null);
+    }
+
+    /**
+     * Enables the task group if a single BasicMarkerNode is in the lookup.
+     * @param ev ignored
+     */
+    @Override
+    public void resultChanged(LookupEvent ev) {
+        RibbonContextualTaskGroup group = getTaskGroup();
+        if (group != null) {
+            JRibbon ribbon = Lookup.getDefault().lookup(RibbonManager.class).getRibbon();
+            // Enable and show the task pane when a single marker is selected.
+            if (this.lookupResult.allInstances().size() == 1) {
+                ribbon.setVisible(group, true);
+                ribbon.setSelectedTask(group.getTask(0)); // use the first (and only) task.
+            } else {
+                ribbon.setVisible(group, false);
+            }
+        }
     }
 
     private RibbonContextualTaskGroup getTaskGroup() {
@@ -47,7 +69,7 @@ public final class MarkerTools implements LookupListener {
             // Find the Marker Tools task group
             for (int i = 0; i < ribbon.getContextualTaskGroupCount(); i++) {
                 RibbonContextualTaskGroup group = ribbon.getContextualTaskGroup(i);
-                if (group.getTitle().equalsIgnoreCase("Marker Tools")) {
+                if (group.getTitle().equalsIgnoreCase(Bundle.CTL_MarkerTools())) {
                     this.taskGroup = group;
                 }
             }
@@ -56,23 +78,7 @@ public final class MarkerTools implements LookupListener {
     }
 
     /**
-     * Enables the task group if a single BasicMarkerNode is in the lookup.
-     *
-     * @param ev ignored
-     */
-    @Override
-    public void resultChanged(LookupEvent ev) {
-        RibbonContextualTaskGroup group = getTaskGroup();
-        if (group != null) {
-            JRibbon ribbon = Lookup.getDefault().lookup(RibbonManager.class).getRibbon();
-            // Enable the tools task paine a single marker is selected.
-            ribbon.setVisible(group, this.lookupResult.allInstances().size() == 1);
-        }
-    }
-
-    /**
      * Gets the singleton instance.
-     *
      * @return singleton
      */
     public static MarkerTools getInstance() {
