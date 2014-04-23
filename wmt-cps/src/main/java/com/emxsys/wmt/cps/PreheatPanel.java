d10456d3c4b3a0c24b2111a24188360bd8f36751
Bruce Schubert <devnull@localhost>
2014-04-23 12:10:48 -0700

    CPS: Added Primary Forces compass charts. Work in progress.

--- wmt-cps/src/main/java/com/emxsys/wmt/cps/PreheatPanel.java
+++ wmt-cps/src/main/java/com/emxsys/wmt/cps/PreheatPanel.java
@@ -33,20 +33,25 @@ import com.emxsys.wmt.cps.charts.ChartUtil;
 import com.emxsys.wmt.gis.api.Coord3D;
 import com.emxsys.wmt.solar.api.SolarUtil;
 import com.emxsys.wmt.solar.spi.DefaultSunlightProvider;
+import com.emxsys.wmt.util.AngleUtil;
 import java.awt.Color;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CompassPlot;
 import org.jfree.data.general.DefaultValueDataset;
+import org.openide.util.NbBundle;
 
 /**
  *
  * @author Bruce Schubert
  */
-public class PreheatPanel extends javax.swing.JPanel {
+@NbBundle.Messages({
+    "CTL_PreheatChartTitle=Solar Heating",
+})public class PreheatPanel extends javax.swing.JPanel {
 
-    private Date date;
     private JFreeChart compassChart;
 
     /**
@@ -55,13 +60,14 @@ public class PreheatPanel extends javax.swing.JPanel {
     public PreheatPanel() {
         initComponents();
         initChartPanel();
+        updateChart(Date.from(Instant.now()));
     }
 
     private void initChartPanel() {
         // Create a compass chart to depict aspect
         compassChart = ChartUtil.createCommonCompassChart(
-                "Solar Heating", null, ChartUtil.WIND_NEEDLE, Color.ORANGE);
-        jPanel1.add(new ChartPanel(compassChart));
+                Bundle.CTL_PreheatChartTitle(), null, ChartUtil.WIND_NEEDLE, Color.ORANGE);
+        jPanelChart.add(new ChartPanel(compassChart));
     }
 
     /**
@@ -74,32 +80,21 @@ public class PreheatPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        jPanelChart = new javax.swing.JPanel();
 
         setBorder(javax.swing.BorderFactory.createTitledBorder(org.openide.util.NbBundle.getMessage(PreheatPanel.class, "PreheatPanel.border.title"))); // NOI18N
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 372, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 252, Short.MAX_VALUE)
-        );
+        jPanelChart.setLayout(new javax.swing.BoxLayout(jPanelChart, javax.swing.BoxLayout.LINE_AXIS));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanelChart, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jPanelChart, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -108,19 +103,19 @@ public class PreheatPanel extends javax.swing.JPanel {
     {
         Coord3D sunPosition = DefaultSunlightProvider.getInstance().getSunPosition(date);
 //        SolarUtil.LocalSolarTimeFromClockTime(date, WIDTH, FRAMEBITS, WIDTH)
-//        // Update Aspect panel
-//        CompassPlot compassPlot = (CompassPlot) compassChart.getPlot();
-//        DefaultValueDataset compassData = (DefaultValueDataset) compassPlot.getDatasets()[0];
-//        double dir = 360. / 24. * hour;
-//        compassData.setValue(dir);
+        // Update Aspect panel
+        CompassPlot compassPlot = (CompassPlot) compassChart.getPlot();
+        DefaultValueDataset compassData = (DefaultValueDataset) compassPlot.getDatasets()[0];
+        int hour = 14;
+        double dir = 360. / 24. * hour;
+        compassData.setValue(dir);
 //        if (hour >= solar.getSunriseHour() && hour <= solar.getSunsetHour())
-//        {
-//            compassChart.setTitle(cardinalPt + " Solar Heating");
-//            compassPlot.setSeriesPaint(0, Color.black);
-//            compassPlot.setSeriesOutlinePaint(0, Color.black);
-//            // Determine cardinal point
-//            String cardinalPt = AngleUtil.degreesToCardinalPoint8(dir);
-//        }
+        {
+            compassPlot.setSeriesPaint(0, Color.black);
+            compassPlot.setSeriesOutlinePaint(0, Color.black);
+            String title = String.format("%1$s Solar Heating", AngleUtil.degreesToCardinalPoint8(dir));
+            compassChart.setTitle(title);
+        }
 //        else
 //        {
 //            compassPlot.setSeriesPaint(0, Color.WHITE);         // arrow heads
@@ -131,6 +126,6 @@ public class PreheatPanel extends javax.swing.JPanel {
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanelChart;
     // End of variables declaration//GEN-END:variables
 }
