d5ac1b0c41b1d815468e0087e1d20c555c8b152f
Bruce Schubert <devnull@localhost>
2014-05-11 17:05:06 -0700

    Globe: Resolves #66. Not sure what was wrong or what was the fix.

--- wmt-globe/src/main/java/com/emxsys/wmt/globe/EmxsysWorldWindManager.java
+++ wmt-globe/src/main/java/com/emxsys/wmt/globe/EmxsysWorldWindManager.java
@@ -32,21 +32,25 @@ package com.emxsys.wmt.globe;
 import com.terramenta.globe.WorldWindManager;
 import com.terramenta.globe.options.GlobeOptions;
 import java.io.File;
+import java.net.URL;
+import java.util.logging.Logger;
 import java.util.prefs.Preferences;
 import org.openide.modules.InstalledFileLocator;
 import org.openide.util.NbPreferences;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
- * A specialized WorldWindManager for the WMT. 
+ * A specialized WorldWindManager for the WMT.
  *
  * @author Bruce Schubert <bruce@emxsys.com>
  */
 @ServiceProvider(service = WorldWindManager.class, position = 1, supersedes = "com.terramenta.globe.WorldWindManager")
 public class EmxsysWorldWindManager extends WorldWindManager {
 
-    public static final String EMXSYS_CONFIG = "modules/worldwind-overrides.xml";
+    public static final String WORLDWIND_CONFIG = "com/emxsys/wmt/globe/worldwind.xml";
+    public static final String WORLDWIND_OVERRIDES = "com/emxsys/wmt/globe/worldwind.xml";
     private static final Preferences prefs = NbPreferences.forModule(GlobeOptions.class);
+    private static final Logger logger = Logger.getLogger(EmxsysWorldWindManager.class.getName());
 
     /**
      * Override WorldWind Configuration Settings with our application defaults while preserving the
@@ -54,11 +58,31 @@ public class EmxsysWorldWindManager extends WorldWindManager {
      * class static block.
      */
     static {
+        // The default World Wind configuration document is config/worldwind.xml.
+        // This can be changed by setting the Java property gov.nasa.worldwind.config.file
+        // to a different file name or a valid URL prior to creating any World Wind object or
+        // invoking any static methods of World Wind classes. If a file is specified its
+        // location must be on the classpath. (The contents of application and WorldWind jar
+        // files are typically on the classpath, in which case the  configuration file may 
+        // be in the jar file.)
+        //URL config = Thread.currentThread().getContextClassLoader().getResource(WORLDWIND_CONFIG);
+        //System.setProperty("gov.nasa.worldwind.config.file", config.toString());
+
+        // Additionally, an application may set another Java property, 
+        // gov.nasa.worldwind.app.config.document, to a file name or URL whose contents contain 
+        // configuration values to override those of the primary configuration document.
+        // World Wind overrides only those values in this application document, it leaves all 
+        // others to the value specified in the primary document. Applications usually specify 
+        // an override document in order to specify the initial layers in the model.
         String config = prefs.get("options.globe.worldwindConfig", "");
-        if (config.isEmpty()) {
-            File file = InstalledFileLocator.getDefault().locate(EMXSYS_CONFIG, "com.emxsys.wmt.globe", false);
-            if (file != null) {
-                System.setProperty("gov.nasa.worldwind.app.config.document", file.getPath());
+        if (!config.isEmpty()) {
+            System.setProperty("gov.nasa.worldwind.app.config.document", config);
+        } else {
+            URL overrides = Thread.currentThread().getContextClassLoader().getResource(WORLDWIND_OVERRIDES);
+            if (overrides != null) {
+                System.setProperty("gov.nasa.worldwind.app.config.document", overrides.toString());
+            } else {
+                logger.severe("Cannot locate resource: " + WORLDWIND_OVERRIDES);
             }
         }
     }
