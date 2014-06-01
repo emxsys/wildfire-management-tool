3b2d4b4206efcce2517cc5534eb525f9a37c30ae
Bruce Schubert <devnull@localhost>
2014-05-31 17:27:19 -0700

    VisAD: Improved TemporalDomain.

--- wmt-visad/src/main/java/com/emxsys/visad/TemporalDomain.java
+++ wmt-visad/src/main/java/com/emxsys/visad/TemporalDomain.java
@@ -37,38 +37,48 @@ import java.time.ZonedDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.openide.util.Exceptions;
+import org.openide.util.NbBundle.Messages;
 import visad.DataImpl;
 import visad.DateTime;
 import visad.Field;
 import visad.FieldImpl;
+import visad.FlatField;
 import visad.FunctionType;
 import visad.Gridded1DDoubleSet;
 import visad.MathType;
 import visad.RealType;
-import visad.SetType;
 import visad.VisADException;
 
 /**
- * A Temporal domain defined by a time set.
+ * A Temporal domain defined by a Gridded1DDoubleSet time set.
  *
  * @author Bruce Schubert <bruce@emxsys.com>
  */
+@Messages({
+    "ERR_TemporalDomainNotInitialized=Domain not initialized."
+})
 public class TemporalDomain {
 
-    private static final RealType TEMPORAL_DOMAIN_TYPE = RealType.Time;
-    private final Gridded1DDoubleSet temporalDomainSet;
+    private static final RealType TIME_DOMAIN_TYPE = RealType.Time;
+    private Gridded1DDoubleSet timeDomainSet;
     private int timeZoneOffset = 0;
-    private static final Logger LOG = Logger.getLogger(TemporalDomain.class.getName());
+    private static final Logger logger = Logger.getLogger(TemporalDomain.class.getName());
+
+    /**
+     * Constructs an uninitialized TemporalDomain. You must invoke initialize(...) before using.
+     */
+    public TemporalDomain() {
+    }
 
     /**
      * Constructs a TemporalDomain beginning at the given start time, for the specified number of
      * hours. The time zone offset is determined the start time ZoneId.
      *
-     * @param start The start time for the domain
+     * @param start Sets the start time for the domain; also sets timezone offset from UTC.
      * @param numHours The number of hours in the domain.
      */
     public TemporalDomain(ZonedDateTime start, int numHours) {
-        this(Times.makeHourlyTimeSet(start, numHours), start.getOffset().getTotalSeconds());
+        initialize(Times.makeHourlyTimeSet(start, numHours), start.getOffset().getTotalSeconds());
     }
 
     /**
@@ -78,15 +88,46 @@ public class TemporalDomain {
      * @param offsetSeconds The time zone offset (from UTC) in seconds.
      */
     public TemporalDomain(Gridded1DDoubleSet timeDomainSet, int offsetSeconds) {
-        this.temporalDomainSet = timeDomainSet;
+        initialize(timeDomainSet, offsetSeconds);
+    }
+
+    /**
+     * (Re)Initializes the TemporalDomain beginning at the given start time, for the specified
+     * number of hours. The time zone offset is determined the start time ZoneId.
+     *
+     * @param start Sets the start time for the domain; also sets timezone offset from UTC.
+     * @param numHours The number of hours in the domain.
+     */
+    public final void initialize(ZonedDateTime start, int numHours) {
+        initialize(Times.makeHourlyTimeSet(start, numHours), start.getOffset().getTotalSeconds());
+    }
+
+    /**
+     * (Re)Initializes the TemporalDomain from a Gridded1DDoubleSet of type RealType.Time
+     *
+     * @param timeDomainSet The time domain in UTC values.
+     * @param offsetSeconds The time zone offset (from UTC) in seconds.
+     */
+    public final void initialize(Gridded1DDoubleSet timeDomainSet, int offsetSeconds) {
+        // TODO: validate the timeDomain parameter
+        this.timeDomainSet = timeDomainSet;
         this.timeZoneOffset = offsetSeconds;
+    }
+
+    /**
+     * @return True if the domain has been initialized and is ready for use.
+     */
+    public boolean isInitialized() {
+        return timeDomainSet != null;
     }
 
     /**
      * Constructor extracts temporal domains from a FieldImpl data type.
      *
      * @param data of type FieldImpl
+     * @deprecated Untested!!
      */
+    @Deprecated
     public TemporalDomain(DataImpl data) {
         try {
             // Validate temporal requirements
@@ -98,7 +139,7 @@ public class TemporalDomain {
             }
             // Extract the time set
             Field field = (Field) data;
-            this.temporalDomainSet = (Gridded1DDoubleSet) field.getDomainSet();
+            timeDomainSet = (Gridded1DDoubleSet) field.getDomainSet();
 
         }
         catch (IllegalArgumentException | VisADException | RemoteException ex) {
@@ -107,17 +148,32 @@ public class TemporalDomain {
         }
     }
 
+    /**
+     * Gets the earliest datetime in the domain.
+     * @return A ZonedDateTime with the same time zone offset as the domain initializer.
+     */
     public ZonedDateTime getStart() {
-        Instant instant = Instant.ofEpochSecond((long) this.temporalDomainSet.getDoubleLowX());
+        if (!isInitialized()) {
+            throw new IllegalStateException(Bundle.ERR_TemporalDomainNotInitialized());
+        }
+        Instant instant = Instant.ofEpochSecond((long) timeDomainSet.getDoubleLowX());
         return ZonedDateTime.ofInstant(instant, ZoneId.ofOffset("", ZoneOffset.ofTotalSeconds(timeZoneOffset)));
     }
 
+    /**
+     * Gets the datetime at the specified array index.
+     * @param index The array index; must be less the domain set's length (e.g., numHours).
+     * @return A ZonedDateTime [time zone offset].
+     */
     public ZonedDateTime getZonedDateTimeAt(int index) {
+        if (!isInitialized()) {
+            throw new IllegalStateException(Bundle.ERR_TemporalDomainNotInitialized());
+        }
         try {
-            if (index < 0 || index >= this.temporalDomainSet.getLength()) {
+            if (index < 0 || index >= timeDomainSet.getLength()) {
                 throw new IllegalArgumentException("Invalid index: " + index);
             }
-            double time = this.temporalDomainSet.getDoubles(false)[0][index];
+            double time = timeDomainSet.getDoubles(false)[0][index];
             Instant instant = Instant.ofEpochSecond((long) time);
             return ZonedDateTime.ofInstant(instant, ZoneId.ofOffset("", ZoneOffset.ofTotalSeconds(timeZoneOffset)));
         }
@@ -127,12 +183,20 @@ public class TemporalDomain {
         return null;
     }
 
+    /**
+     * Gets the VisAD DateTime at the specified index.
+     * @param index The array index; must be less the domain set's length (e.g., numHours).
+     * @return A VisAD DateTime [UTC].
+     */
     public DateTime getDateTimeAt(int index) {
+        if (!isInitialized()) {
+            throw new IllegalStateException(Bundle.ERR_TemporalDomainNotInitialized());
+        }
         try {
-            if (index < 0 || index >= this.temporalDomainSet.getLength()) {
+            if (index < 0 || index >= timeDomainSet.getLength()) {
                 throw new IllegalArgumentException("Invalid index: " + index);
             }
-            double time = this.temporalDomainSet.getDoubles(false)[0][index];
+            double time = timeDomainSet.getDoubles(false)[0][index];
             return Times.fromDouble(time);
         }
         catch (VisADException ex) {
@@ -141,11 +205,20 @@ public class TemporalDomain {
         return null;
     }
 
-    public FieldImpl newTemporalField(MathType range) {
+    /**
+     * Creates a VisAD function represented by a FieldImpl from the temporal domain using the
+     * supplied MathType for the range. The caller must set the range samples.
+     * @param range The MathType to use as the function's range.
+     * @return A FieldImpl with FunctionType (time) -> (range).
+     */
+    public FieldImpl createTemporalField(MathType range) {
+        if (!isInitialized()) {
+            throw new IllegalStateException(Bundle.ERR_TemporalDomainNotInitialized());
+        }
         try {
-            FunctionType functionType = new FunctionType(TEMPORAL_DOMAIN_TYPE, range);
-            FieldImpl field = new FieldImpl(functionType, this.temporalDomainSet);
-            LOG.log(Level.CONFIG, "newTemporalField created: {0}", field.getType().prettyString());
+            FunctionType functionType = new FunctionType(TIME_DOMAIN_TYPE, range);
+            FieldImpl field = new FieldImpl(functionType, timeDomainSet);
+            logger.log(Level.FINE, "createTemporalField created: {0}", field.getType().prettyString());
             return field;
         }
         catch (VisADException ex) {
@@ -154,17 +227,42 @@ public class TemporalDomain {
         }
     }
 
-    public Gridded1DDoubleSet getTemporalDomainSet() {
-        return temporalDomainSet;
+    /**
+     * Creates a simple VisAD function represented by a FlatField from the temporal domain using the
+     * supplied MathType for the range. The caller must set the range samples.
+     * @param range The MathType to use as the function's range.
+     * @return A FieldFlat with FunctionType (time) -> (range).
+     */
+    public FlatField createSimpleTemporalField(MathType range) {
+        if (!isInitialized()) {
+            throw new IllegalStateException(Bundle.ERR_TemporalDomainNotInitialized());
+        }
+        try {
+            FunctionType functionType = new FunctionType(TIME_DOMAIN_TYPE, range);
+            FlatField field = new FlatField(functionType, timeDomainSet);
+            logger.log(Level.FINE, "createSimpleTemporalField created: {0}", field.getType().prettyString());
+            return field;
+        }
+        catch (VisADException ex) {
+            Exceptions.printStackTrace(ex);
+            throw new RuntimeException(ex);
+        }
     }
 
-    public RealType getTemporalDomainType() {
-        return TEMPORAL_DOMAIN_TYPE;
+    public Gridded1DDoubleSet getDomainSet() {
+        return timeDomainSet;
+    }
+
+    public RealType getDomainType() {
+        return TIME_DOMAIN_TYPE;
     }
 
     @Override
     public String toString() {
-        return this.temporalDomainSet.toString();
+        if (timeDomainSet == null) {
+            throw new IllegalStateException(Bundle.ERR_TemporalDomainNotInitialized());
+        }
+        return timeDomainSet.toString();
     }
 
 }
