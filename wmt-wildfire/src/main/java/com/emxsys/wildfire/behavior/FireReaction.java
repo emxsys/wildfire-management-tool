457bbf25f1bc85def11d63a2ec4602ebe63e1cf2
Bruce Schubert <devnull@localhost>
2014-06-12 09:20:18 -0700

    Wildfire: FireReaction snapshot of work in progress.

--- wmt-wildfire/src/main/java/com/emxsys/wildfire/behavior/FireReaction.java
+++ wmt-wildfire/src/main/java/com/emxsys/wildfire/behavior/FireReaction.java
@@ -30,8 +30,23 @@
 package com.emxsys.wildfire.behavior;
 
 import com.emxsys.gis.api.Terrain;
+import com.emxsys.util.AngleUtil;
+import com.emxsys.visad.FireUnit;
 import com.emxsys.weather.api.Weather;
+import static com.emxsys.wildfire.api.WildfireType.ROS;
+import static java.lang.Math.PI;
+import static java.lang.Math.asin;
+import static java.lang.Math.cos;
+import static java.lang.Math.min;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.toDegrees;
+import static java.lang.Math.toRadians;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openide.util.Exceptions;
 import visad.Real;
+import visad.VisADException;
 
 /**
  *
@@ -39,10 +54,24 @@ import visad.Real;
  */
 public class FireReaction {
 
+    private static final Logger logger = Logger.getLogger(FireReaction.class.getName());
     private final FuelBed fuelBed;
     private final Terrain terrain;
     private final Real windSpd;
     private final Real windDir;
+
+    private Real rateOfSpread;
+    private Real rateOfSpreadMax;
+    private Real spreadDirectionMax;
+    private Real effectiveWindSpeed;
+
+    private double phiEw;           // Combined wind and slope factors
+    private double effectiveWnd;    // Effective wind speed [ft/min]
+    private double spreadDir;       // Spread direction [degrees]
+
+    static {
+        logger.setLevel(Level.ALL);
+    }
 
     public static FireReaction from(FuelBed fuelBed, Terrain terrain, Weather weather) {
 
@@ -56,7 +85,126 @@ public class FireReaction {
         this.windDir = windDir;
     }
 
+    public Real getRateOfSpreadNoWindNoSlope() {
+
+        if (this.rateOfSpread == null) {
+            double ros = Rothermel.rateOfSpreadNoWindNoSlope(
+                    fuelBed.getReactionIntensity().getValue(),
+                    fuelBed.getPropagatingFluxRatio().getValue(),
+                    fuelBed.getHeatSink().getValue());
+            this.rateOfSpread = new Real(ROS, ros);
+
+            try {
+                System.out.println(String.format("ROS [%1$s] (Wind=0,Slope=0): {%2$s} [chn/hr]",
+                        fuelBed.getFuelModel().getModelCode(),
+                        rateOfSpread.getValue(FireUnit.chain_hour)));
+                if (logger.isLoggable(Level.FINE)) {
+                    logger.log(Level.FINE, "ROS [{0}] (Wind=0,Slope=0): {1} [chn/hr]",
+                            new Object[]{fuelBed.getFuelModel().getModelCode(),
+                                         rateOfSpread.getValue(FireUnit.chain_hour)});
+                }
+            } catch (VisADException ex) {
+            }
+        }
+        return rateOfSpread;
+    }
+
     public Real getRateOfSpread() {
-        return null;
+        if (this.rateOfSpreadMax == null) {
+
+            calcWindAndSlopeEffects(windSpd, windDir, terrain.getAspect(), terrain.getSlope());
+
+            double ros0 = getRateOfSpreadNoWindNoSlope().getValue();
+            double rosMax = 0;
+            double dirMax = 0;
+            if (phiEw <= 0) {
+                // No wind, no slope
+                rosMax = ros0;
+            } else {
+                rosMax = ros0 * (1 + phiEw);
+                dirMax = spreadDir;
+            }
+            this.rateOfSpreadMax = new Real(ROS, rosMax);
+        }
+        return this.rateOfSpreadMax;
+
+    }
+
+    /**
+     * Calculates the combined wind and slope effects, including the wind and slope coefficients,
+     * and the direction of maximum spread.
+     *
+     * Both the wind coefficient (phi W) and the slope coefficient (phi S) have the effect of
+     * increasing the proportion of heat reaching the adjacent fuel. They act as multipliers of the
+     * reaction intensity.
+     *
+     * Rothermel 1972: eq. (44)
+     *
+     * @param windSpd The 20' wind speed.
+     * @param windDir The customary wind direction: the true-north angle the wind is FROM [degrees].
+     * @param aspect The terrain aspect: e.g., a South aspect would have a 180 degree value
+     * [degrees].
+     * @param slope The terrain slope angle [degrees].
+     *
+     */
+    protected void calcWindAndSlopeEffects(Real windSpd, Real windDir, Real aspect, Real slope) {
+
+        try {
+            double sigma = fuelBed.getCharacteristicSAV().getValue();
+            double beta_ratio = fuelBed.getRelativePackingRatio().getValue();
+            double I_r = fuelBed.getReactionIntensity().getValue();
+
+            // Get wind and slope cooefficients (phiW and phiS)
+            double windFactor = Rothermel.windFactor(
+                    windSpd.getValue(FireUnit.ft_min),
+                    sigma, beta_ratio);
+            double slopeFactor = Rothermel.slopeFactor(
+                    terrain.getSlopeDegrees(),
+                    fuelBed.getMeanPackingRatio().getValue());
+
+            // Convert wind direction to where the is blowing TO (i.e., spread direction)
+            double wndDir = AngleUtil.normalize360(windDir.getValue() + 180);
+
+            // Convert terrain aspect to an upslope direction
+            double slpDir = AngleUtil.normalize360(aspect.getValue() + 180);
+
+            // Get the angle between the wind vector and the upslope vector
+            double split = AngleUtil.angularDistanceBetween(wndDir, slpDir);
+
+            double slpRad = toRadians(slpDir);
+            double splitRad = toRadians(split);
+
+            // Get the combined wind and slope vector components for max ROS
+            double vx = slopeFactor + (windFactor * cos(splitRad));
+            double vy = windFactor * sin(splitRad);
+            double vl = sqrt(vx * vx + vy * vy); // new combined wind and slope coeeficient
+
+            // Calculate direction of maximum spread
+            double aRad = asin(vy / vl);    // vector direction relative to upslope
+            double dirRad;                  // vector direction rotated to compass angle
+            if (vx >= 0.) {
+                dirRad = (vy >= 0.)
+                        ? slpRad + aRad
+                        : slpRad + aRad + 2 * PI;
+            } else {
+                dirRad = slpRad - aRad + PI;
+            }
+            // Spread direction [degrees]
+            spreadDir = AngleUtil.normalize360(toDegrees(dirRad));
+
+            // Combined wind and slope factors
+            phiEw = vl;
+
+            // Effective windspeed [ft/min]
+            effectiveWnd = Rothermel.effectiveWindSpeed(phiEw, beta_ratio, sigma);
+            // Rothermel eq. (87) sets an upper limit on the wind multiplication factor
+            if (effectiveWnd > 0.9 * I_r) {
+                effectiveWnd = min(effectiveWnd, 0.9 * I_r);
+                phiEw = Rothermel.windFactor(effectiveWnd, sigma, beta_ratio);
+            }
+        } catch (VisADException ex) {
+            Exceptions.printStackTrace(ex);
+            throw new IllegalStateException(ex);
+        }
     }
 }
